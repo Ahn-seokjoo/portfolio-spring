11201c57b83d6050ebdd03a35a05b0f69913a50d
Ahn-seokjoo <tjrwn0716@naver.com>
2025-03-01 16:15:49 +0900

    [feature] 공통 에러 처리

--- src/main/kotlin/com/seokjoo/portfolio/admin/advice/AdminApiControllerAdvice.kt
+++ src/main/kotlin/com/seokjoo/portfolio/admin/advice/AdminApiControllerAdvice.kt
@@ -1,7 +1,36 @@
 package com.seokjoo.portfolio.admin.advice
 
+import com.seokjoo.portfolio.admin.exception.AdminException
+import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class AdminApiControllerAdvice {
+    private val logger = LoggerFactory.getLogger(AdminApiControllerAdvice::class.java)
+
+    @ExceptionHandler
+    fun handleException(e: AdminException): ResponseEntity<String> {
+        logger.info(e.message, e)
+        return ResponseEntity.status(e.httpStatus).body(e.message)
+    }
+
+    @ExceptionHandler
+    fun handleException(e: MethodArgumentNotValidException): ResponseEntity<String> {
+        logger.info(e.message, e)
+
+        val fieldError = e.bindingResult.fieldErrors.getOrNull(0)
+        val message = "[${fieldError?.field} ${fieldError?.defaultMessage}]"
+
+        return ResponseEntity.badRequest().body(message)
+    }
+
+    @ExceptionHandler
+    fun handleException(e: Exception): ResponseEntity<String> {
+        logger.info(e.message, e)
+
+        return ResponseEntity.internalServerError().body("시스템 오류 발생")
+    }
 }
--- src/main/kotlin/com/seokjoo/portfolio/admin/exception/AdminException.kt
+++ src/main/kotlin/com/seokjoo/portfolio/admin/exception/AdminException.kt
@@ -6,3 +6,8 @@ abstract class AdminException(
     val httpStatus: HttpStatus,
     message: String,
 ) : RuntimeException(message)
+
+class AdminBadRequestException(message: String) : AdminException(httpStatus = HttpStatus.BAD_REQUEST, message = message)
+
+class AdminInternalServerErrorException(message: String) :
+    AdminException(httpStatus = HttpStatus.INTERNAL_SERVER_ERROR, message = message)
